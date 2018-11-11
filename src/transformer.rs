2076660cede776bdf4c1f8b1ad75bcfcc54729a2
Klas Segeljakt <klasseg@kth.se>
2018-11-11 22:42:45 +0100

    Try put Op inside of Expr

--- src/transformer.rs
+++ src/transformer.rs
@@ -10,24 +10,10 @@ fn span_into_str(span: Span) -> &str {
 
 #[derive(Debug, FromPest)]
 #[pest_ast(rule(Rule::program))]
-pub struct SyntaxTree<'a> (
-  Expr<'a>,
-  EOI
-);
-
-#[derive(Debug, FromPest)]
-#[pest_ast(rule(Rule::function))]
-pub struct Function<'a> (
-  Ident<'a>,
-  Vec<Param<'a>>,
-  Expr<'a>,
-);
-
-#[derive(Debug, FromPest)]
-#[pest_ast(rule(Rule::param))]
-pub struct Param<'a> (
-  Ident<'a>,
-);
+pub struct SyntaxTree<'a> {
+  expr: Expr<'a>,
+  eoi: EOI
+}
 
 #[derive(Debug, FromPest)]
 #[pest_ast(rule(Rule::ident))]
@@ -36,11 +22,19 @@ pub struct Ident<'a> (
   &'a str
 );
 
-#[derive(Debug, FromPest)]
-#[pest_ast(rule(Rule::expr))]
+#[derive(Debug)]
 pub enum Expr<'a> {
-  Let(Let<'a>),
-  Op(Op<'a>),
+  LetExpr(Let<'a>),
+  I32(i32),
+  Bool(bool),
+  Term(&'a str),
+  Not(Box<Expr<'a>>),
+  Neg(Box<Expr<'a>>),
+  Add(Box<Expr<'a>>, Box<Expr<'a>>),
+  Sub(Box<Expr<'a>>, Box<Expr<'a>>),
+  Mul(Box<Expr<'a>>, Box<Expr<'a>>),
+  Div(Box<Expr<'a>>, Box<Expr<'a>>),
+  Pow(Box<Expr<'a>>, Box<Expr<'a>>),
 }
 
 #[derive(Debug, FromPest)]
@@ -54,23 +48,8 @@ pub struct Let<'a> (
 #[pest_ast(rule(Rule::binding))]
 pub struct Binding<'a> (
   Ident<'a>,
-  Op<'a>,
+  Box<Expr<'a>>,
 );
-
-
-#[derive(Debug)]
-pub enum Op<'a> {
-  I32(i32),
-  Bool(bool),
-  Term(&'a str),
-  Not(Box<Op<'a>>),
-  Neg(Box<Op<'a>>),
-  Add(Box<Op<'a>>, Box<Op<'a>>),
-  Sub(Box<Op<'a>>, Box<Op<'a>>),
-  Mul(Box<Op<'a>>, Box<Op<'a>>),
-  Div(Box<Op<'a>>, Box<Op<'a>>),
-  Pow(Box<Op<'a>>, Box<Op<'a>>),
-}
 
 #[derive(Debug, FromPest)]
 #[pest_ast(rule(Rule::EOI))]
@@ -93,32 +72,35 @@ use from_pest::FromPest;
 use from_pest::ConversionError;
 use void::Void;
 
-type OpResult<'a> = Result<Op<'a>, ConversionError<Void>>;
-impl<'a> FromPest<'a> for Op<'a> {
+type ExprResult<'a> = Result<Expr<'a>, ConversionError<Void>>;
+impl<'a> FromPest<'a> for Expr<'a> {
   type Rule = Rule;
   type FatalError = Void;
-  fn from_pest(pest: &mut Pairs<'a, Rule>) -> OpResult<'a> {
+  fn from_pest(pest: &mut Pairs<'a, Rule>) -> ExprResult<'a> {
     use self::Rule::*;
-    use self::Op::*;
-    PREC_CLIMBER.climb(pest,
-      |pair: Pair<Rule>| Ok(
+    //let pest = pest.next().unwrap().into_inner();
+    println!("{:?}", pest);
+    let res = PREC_CLIMBER.climb(pest,
+      |pair: Pair<Rule>| Ok( {println!("{:?}\n", pair.as_rule());
         match pair.as_rule() {
-          lit_i32  => Op::I32(pair.as_str().parse().unwrap()),
-          lit_bool => Op::Bool(pair.as_str().parse().unwrap()),
-          term     => Op::Term(pair.as_str()),
-          not      => Op::Not(box Op::from_pest(&mut pair.into_inner())?),
-          neg      => Op::Neg(box Op::from_pest(&mut pair.into_inner())?),
-          op       => Op::from_pest(&mut pair.into_inner())?,
+          lit_i32  => Expr::I32(pair.as_str().parse().unwrap()),
+          lit_bool => Expr::Bool(pair.as_str().parse().unwrap()),
+          term     => Expr::Term(pair.as_str()),
+          not      => Expr::Not(box Expr::from_pest(&mut pair.into_inner())?),
+          neg      => Expr::Neg(box Expr::from_pest(&mut pair.into_inner())?),
+          let_expr => Expr::LetExpr(Let::from_pest(&mut Pairs::single(pair))?),
+          expr     => Expr::from_pest(&mut pair.into_inner())?,
           _ => unreachable!(),
-        }),
-      |lhs: OpResult, op: Pair<Rule>, rhs: OpResult| Ok(
+        }}),
+      |lhs: ExprResult, op: Pair<Rule>, rhs: ExprResult| Ok(
         match op.as_rule() {
-          add => Add(box lhs?, box rhs?),
-          sub => Sub(box lhs?, box rhs?),
-          mul => Mul(box lhs?, box rhs?),
-          div => Div(box lhs?, box rhs?),
-          pow => Pow(box lhs?, box rhs?),
+          add => Expr::Add(box lhs?, box rhs?),
+          sub => Expr::Sub(box lhs?, box rhs?),
+          mul => Expr::Mul(box lhs?, box rhs?),
+          div => Expr::Div(box lhs?, box rhs?),
+          pow => Expr::Pow(box lhs?, box rhs?),
           _ => unreachable!(),
-        }))
+        }));
+    println!("{:?}", res); res
   }
 }
